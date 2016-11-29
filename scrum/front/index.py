c474c5f482e64873de7e4eaa1f54c1221dac9ccf
Francisco Carrizales <fcarrizalest@gmail.com>
2016-11-28 23:20:08 -0600

    make it better

--- scrum/front/index.py
+++ scrum/front/index.py
@@ -71,20 +71,20 @@ def index():
 	sql = " SELECT projects.name as PName, \
 				  iterations.name, \
 				  iterations.end_date, \
-				  coalesce( NULLIF(iterations.story_count,''),0), \
+				  coalesce( NULLIF(iterations.story_count,0),0), \
 				  ( \
-				  	  SELECT coalesce( NULLIF( SUM( stories.points ),'' ) ,0)\
+				  	  SELECT coalesce( NULLIF( SUM( stories.points ),0 ) ,0)\
 				  	  FROM stories \
 				  	  WHERE stories.iteration_id = iterations.id \
 				   ) as SPoints, \
 				   	( \
-				  	  SELECT coalesce( NULLIF( SUM( stories.points ) ,'') , 0) \
+				  	  SELECT coalesce( NULLIF( SUM( stories.points ) ,0) , 0) \
 				  	  FROM stories \
 				  	  WHERE stories.iteration_id = iterations.id AND\
 				  	  		stories.all_labels = 'Bug' \
 				   ) as SBPoints, \
 					( \
-				  	  SELECT coalesce( NULLIF( COUNT( stories.id ),'') , 0) \
+				  	  SELECT coalesce( NULLIF( COUNT( stories.id ),0) , 0) \
 				  	  FROM stories \
 				  	  WHERE stories.iteration_id = iterations.id AND\
 				  	  		stories.all_labels = 'Bug' \
