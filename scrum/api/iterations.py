bd4dd2697d740fa76a3fb15f1cd3456115c3dc43
Francisco Carrizales <fcarrizalest@gmail.com>
2017-01-06 09:43:05 -0600

    Creamos servicio de iteracion actual, para no depender de una fecha

--- scrum/api/iterations.py
+++ scrum/api/iterations.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 
 from ..services import projects,iterations,stories
-
+from ..core import db
 from . import route
+
+from sqlalchemy.sql import text
 
 
 bp = Blueprint('iterations', __name__, url_prefix='/iterations')
@@ -33,6 +35,20 @@ def iteration_stories(id):
     
     
     return iteration.stories.all()
+
+
+@route(bp, '/actual')
+def actual( ):
+    select_list = db.engine.execute(text('SELECT iterations.end_date \
+                                     FROM iterations \
+                                     GROUP BY iterations.end_date order by iterations.end_date DESC limit 1'))
+    str_date = ""
+    for row in select_list:
+        str_date = row['end_date']
+
+    
+    
+    return iterations.find( end_date= str_date ).all()
 
 
 @route(bp, '/<str_date>')
